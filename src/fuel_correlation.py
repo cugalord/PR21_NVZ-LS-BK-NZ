75b67cf23812d2f7fffec7f1d84958a0e46f6a7a
LukaSveigl <lukasveigl2708@gmail.com>
2022-04-09 12:58:51 +0200

    Finished first task

--- src/fuel_correlation.py
+++
@@ -1,6 +0,0 @@
-"""
-Module that contains functions used to answer the second question of this project, which is
-finding possible correlations between different fuel prices.
-"""
-
-from setup import data_extraction as de
